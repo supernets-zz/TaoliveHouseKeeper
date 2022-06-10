4ca54eb47157793e9627628a65752794263ee448
supernets-zz <supernets@gmail.com>
2022-06-10 22:37:43 +0800

    睡觉赚元宝、每日签到

---
+++ signInEarn.js
@@ -0,0 +1,180 @@
+var signInEarn = {};
+
+var common = require("./common.js");
+var commonAction = require("./commonAction.js");
+
+const signInTag = "元宝中心每日签到";   //null-状态未知, done-已完成, out of time-过期
+const getSignInBonusTag = "元宝中心每日签到开奖";
+
+signInEarn.dailyJobs = [];
+signInEarn.dailyJobs.push(signInTag);
+
+signInEarn.doSignIn = function () {
+    log("doSignIn");
+    var nowDate = new Date().Format("yyyy-MM-dd");
+    var done = common.safeGet(nowDate + ":" + signInTag);
+    if (done != null) {
+        log(signInTag + " 已做: " + done);
+        if (done == "done") {
+            common.grantWalkToEarnPermission();
+        }
+        return;
+    }
+
+    //正常收益下签到才会成功，否则就是一直没签到
+    toast("doSignIn");
+    // 我的-> 元宝中心-> 去签到，上划屏幕找到去签到或待开奖按钮
+    if (!commonAction.gotoCoinCenter()) {
+        commonAction.backTaoliveMainPage();
+        return;
+    }
+
+    var btnSignIn = null;
+    for (var i = 0; i < 10; i++) {
+        btnSignIn = textMatches(/去签到|待开奖/).visibleToUser(true).findOne(1000);
+        if (btnSignIn == null || btnSignIn != null && btnSignIn.bounds().height() < 42) {
+            log("上划屏幕找 去签到|待开奖: " + swipe(device.width / 2, device.height * 7 / 8, device.width / 2, device.height / 8, 1000));
+            sleep(1000);
+            continue;
+        }
+        break;
+    }
+
+    if (btnSignIn == null) {
+        log("去签到|待开奖 not found")
+        commonAction.backTaoliveMainPage();
+        return;
+    }
+
+    //去签到按钮变为待开奖说明已经签过到了
+    if (btnSignIn.text() == "待开奖") {
+        common.safeSet(nowDate + ":" + signInTag, "done");
+        toastLog("完成 " + signInTag);
+        common.grantWalkToEarnPermission();
+        commonAction.backTaoliveMainPage();
+        return;
+    }
+
+    // 签到时间[00:00~20:00)
+    var now = new Date().getTime();
+    var curDate = new Date().Format("yyyy/MM/dd");
+    var signInBeginTime = new Date(curDate + " 00:00:00").getTime();
+    var signInEndTime = new Date(curDate + " 19:59:59").getTime();
+    log("签到有效时间段: [" + common.timestampToTime(signInBeginTime) + ", " + common.timestampToTime(signInEndTime) + "]");
+    //不在时间范围内，签到任务又没做，当日就断签了
+    if (now < signInBeginTime || now >= signInEndTime) {
+        common.safeSet(nowDate + ":" + signInTag, "out of time");
+        toastLog("过期 " + signInTag);
+        commonAction.backTaoliveMainPage();
+        return;
+    }
+
+    log("点击 去签到: " + click(btnSignIn.bounds().centerX(), btnSignIn.bounds().centerY()));
+
+    //提示语"签到提醒"
+    var signInReminder = common.waitForText("text", "签到提醒", true, 10);
+    var signInFrame = signInReminder.parent();
+    var signInCalendar = signInFrame.child(signInFrame.childCount() - 1);   //最后一个子节点
+    //有14个子节点，遍历一遍，子节点可点击，要找其最后一个子节点为view且日期与当日相同的点击，最后一个有点特殊(depth(18)可点击)，比前面的13个(depth(17)可点击)多一层
+    for (var i = 0; i < signInCalendar.childCount(); i++) {
+        var signInItem = signInCalendar.child(i);   //可点击
+        //连续14天签到的前13天签到
+        if (i != signInCalendar.childCount() - 1) {
+            if (signInItem.child(signInItem.childCount() - 1).className() == "android.view.View") {
+                log(new Date().Format("MM.dd") + ", " + signInItem.child(signInItem.childCount() - 1).text());
+                if (new Date().Format("MM.dd") == signInItem.child(signInItem.childCount() - 1).text()) {
+                    log(new Date().Format("MM.dd") + " 签到: " + signInItem.click());
+                    sleep(1000);
+                    common.safeSet(nowDate + ":" + signInTag, "done");
+                    toastLog("完成 " + signInTag);
+                    common.grantWalkToEarnPermission();
+                    break;
+                }
+            } else {
+                log("已签");
+            }
+        } else {    //连续14天签到的最后一天签到
+            var lastSignInItem = signInItem.child(0);   //可点击
+            if (lastSignInItem.child(lastSignInItem.childCount() - 1).className() == "android.view.View") {
+                log(new Date().Format("MM.dd") + ", " + lastSignInItem.child(lastSignInItem.childCount() - 1).text());
+                if (new Date().Format("MM.dd") == lastSignInItem.child(lastSignInItem.childCount() - 1).text()) {
+                    log(new Date().Format("MM.dd") + " 签到: " + lastSignInItem.click());
+                    sleep(1000);
+                    common.safeSet(nowDate + ":" + signInTag, "done");
+                    toastLog("完成 " + signInTag);
+                    common.grantWalkToEarnPermission();
+                    break;
+                }
+            } else {
+                log("已签");
+            }
+        }
+    }
+
+    commonAction.backTaoliveMainPage();
+}
+
+signInEarn.doGetSignInBonus = function () {
+    log("doGetSignInBonus");
+    var nowDate = new Date().Format("yyyy-MM-dd");
+    var done = common.safeGet(nowDate + ":" + getSignInBonusTag);
+    if (done != null) {
+        log(getSignInBonusTag + " 已做: " + done);
+        return;
+    }
+
+    //看签到了没，没签到也开不了奖
+    var signed = common.safeGet(nowDate + ":" + signInTag);
+    if (signed != "done") {
+        log(signInTag + " 状态: " + signed);
+        return;
+    }
+
+    // 开奖时间[20:00~24:00)
+    var now = new Date().getTime();
+    var curDate = new Date().Format("yyyy/MM/dd");
+    var getBonusBeginTime = new Date(curDate + " 20:00:00").getTime();
+    var getBonusEndTime = new Date(curDate + " 23:59:59").getTime();
+    log("签到开奖有效时间段: [" + common.timestampToTime(getBonusBeginTime) + ", " + common.timestampToTime(getBonusEndTime) + "]");
+    if (now < getBonusBeginTime || now >= getBonusEndTime) {
+        log("未到开奖时间");
+        return;
+    }
+
+    //正常收益下开奖才能获取收益
+    toast("doGetSignInBonus");
+    // 我的-> 元宝中心-> 待开奖，上划屏幕找到去签到或待开奖按钮
+    if (!commonAction.gotoCoinCenter()) {
+        commonAction.backTaoliveMainPage();
+        return;
+    }
+
+    var btnSignIn = null;
+    for (var i = 0; i < 10; i++) {
+        btnSignIn = textMatches(/去签到|待开奖/).visibleToUser(true).findOne(1000);
+        if (btnSignIn == null || btnSignIn != null && btnSignIn.bounds().height() < 42) {
+            log("上划屏幕找 去签到|待开奖: " + swipe(device.width / 2, device.height * 7 / 8, device.width / 2, device.height / 8, 1000));
+            sleep(1000);
+            continue;
+        }
+        break;
+    }
+
+    if (btnSignIn == null) {
+        log("去签到|待开奖 not found")
+        commonAction.backTaoliveMainPage();
+        return;
+    }
+
+    log("点击 待开奖: " + click(btnSignIn.bounds().centerX(), btnSignIn.bounds().centerY()));
+
+    //每日签到开奖
+    var getBonusTips = common.waitForText("text", "最高可获得1000000元宝", true, 10);
+    var bonusFrame = getBonusTips.parent();
+    log(bonusFrame.child(bonusFrame.childCount() - 1).click());
+    sleep(1000);
+
+    commonAction.backTaoliveMainPage();
+}
+
+module.exports = signInEarn;
---
+++ sleepToEarn.js
@@ -0,0 +1,211 @@
+var sleepToEarn = {};
+
+var common = require("./common.js");
+var commonAction = require("./commonAction.js");
+
+const browseTag = "睡觉赚元宝浏览首页";
+const sleepTag = "睡觉赚元宝去睡觉";    //每晚21:00至次日3:00
+const wakeupTag = "睡觉赚元宝我醒来啦"; //次日06:00至20:59，同连续3天完成睡觉任务领取福袋
+
+sleepToEarn.dailyJobs = [];
+sleepToEarn.dailyJobs.push(browseTag);
+
+gotoSleepEarnCoins = function () {
+    if (!commonAction.gotoCoinCenter()) {
+        return null;
+    }
+
+    var btnSleep = null;
+    for (var i = 0; i < 10; i++) {
+        btnSleep = text("去睡觉").visibleToUser(true).findOne(1000);
+        if (btnSleep == null || btnSleep != null && btnSleep.bounds().height() < 42) {
+            log("上划屏幕找 去睡觉: " + swipe(device.width / 2, device.height * 7 / 8, device.width / 2, device.height / 8, 1000));
+            sleep(1000);
+            continue;
+        }
+        break;
+    }
+
+    if (btnSleep == null) {
+        return null;
+    }
+
+    log("点击 去睡觉: " + click(btnSleep.bounds().centerX(), btnSleep.bounds().centerY()));
+
+    //提示语"Hi，欢迎来领元宝...|Hi，上午好"
+    // var timeTips = common.waitForTextMatches(/现在是\d+月\d+日.*/, true, 10);
+    var welcomeTips = common.waitForTextMatches(/Hi，.*/, true, 10);
+    return welcomeTips;
+}
+
+sleepToEarn.doSleepMainBrowse = function () {
+    log("doSleepMainBrowse");
+    // 我的-> 元宝中心-> 去睡觉，先上划个半屏，看有没有浏览商品30秒，时有时无的，有的时候才做
+    var nowDate = new Date().Format("yyyy-MM-dd");
+    var done = common.safeGet(nowDate + ":" + browseTag);
+    if (done != null) {
+        log(browseTag + " 已做: " + done);
+        return;
+    }
+
+    toast("doSleepMainBrowse");
+    if (gotoSleepEarnCoins() == null) {
+        commonAction.backTaoliveMainPage();
+        return;
+    }
+
+    sleep(1000);
+    log("往上划动半个屏幕: " + swipe(device.width / 2, device.height * 7 / 8, device.width / 2, device.height / 8, 1000));
+    var walkTips = textContains("浏览页面").findOne(1000);
+    if (walkTips != null) {
+        log("等待 商品 浏览完成，60s超时");
+        var ret = commonAction.scrollThrough("浏览页面", 60);
+        if (ret) {
+            common.safeSet(nowDate + ":" + browseTag, "done");
+            toastLog("完成 " + browseTag);
+        } else {
+            toastLog("60s timeout");
+        }
+    } else {
+        toastLog("没有 浏览页面");
+    }
+
+    commonAction.backTaoliveMainPage();
+}
+
+sleepToEarn.doSleep = function () {
+    log("doSleep");
+    // 我的-> 元宝中心-> 去睡觉
+    var nowDate = new Date().Format("yyyy-MM-dd");
+    var done = common.safeGet(nowDate + ":" + sleepTag);
+    if (done != null) {
+        log(sleepTag + " 已做: " + done);
+        return;
+    }
+
+    // 睡觉时间[21:00~03:00)
+    var now = new Date().getTime();
+    var curDate = new Date().Format("yyyy/MM/dd");
+    var sleepBeginTime = new Date(curDate + " 06:00:00").getTime();
+    var sleepEndTime = new Date(curDate + " 20:59:59").getTime();
+    log("唤醒有效时间段: [" + common.timestampToTime(sleepBeginTime) + ", " + common.timestampToTime(sleepEndTime) + "]");
+    //不在时间范围内不判断唤醒任务做没做
+    if (now < sleepBeginTime || now >= sleepEndTime) {
+        return;
+    }
+
+    toast("doWakeup");
+    var welcomeTips = gotoSleepEarnCoins();
+    if (welcomeTips == null) {
+        commonAction.backTaoliveMainPage();
+        return;
+    }
+
+    var mainFrame = welcomeTips.parent().parent();
+    var wakeupBtn = mainFrame.child(1);
+    var lucyPackageBtn = mainFrame.child(2).child(0);
+    log("睡觉按钮: " + wakeupBtn.text())
+    if (wakeupBtn.text() == "我醒来啦") {
+        var clickRet = wakeupBtn.click();
+        log("点击 我醒来啦: " + clickRet);
+        if (clickRet) {
+            common.safeSet(nowDate + ":" + wakeupTag, "done");
+            toastLog("完成 " + wakeupTag);
+
+            //关闭提示
+            var doneTips = text("完成睡觉任务").findOne(1000);
+            if (doneTips != null) {
+                var doneDlg = doneTips.parent();
+                var clickRet = doneDlg.child(doneDlg.childCount() - 1).click();
+                log("关闭 完成睡觉任务: " + clickRet);
+            }
+
+            //点福袋
+            log("福袋进度: " + lucyPackageBtn.text());
+            if (lucyPackageBtn.text() == "2/3") {
+                var clickRet = lucyPackageBtn.click();
+                sleep(1000);
+                var doneBtn = text("我知道啦").findOne(1000);
+                if (doneBtn != null) {
+                    clickRet = doneBtn.click();
+                    log("关闭 领取福袋: " + clickRet);
+                }
+            }
+        }
+    } else {
+        common.safeSet(nowDate + ":" + wakeupTag, "awake");
+        toastLog("醒着呢 " + wakeupTag);
+    }
+
+    commonAction.backTaoliveMainPage();
+}
+
+sleepToEarn.doWakeup = function () {
+    log("doWakeup");
+    // 我的-> 元宝中心-> 去睡觉
+    var nowDate = new Date().Format("yyyy-MM-dd");
+    var done = common.safeGet(nowDate + ":" + wakeupTag);
+    if (done != null) {
+        log(wakeupTag + " 已做: " + done);
+        return;
+    }
+
+    // 唤醒时间[06:00~20:59)
+    var now = new Date().getTime();
+    var curDate = new Date().Format("yyyy/MM/dd");
+    var wakeupBeginTime = new Date(curDate + " 06:00:00").getTime();
+    var wakeupEndTime = new Date(curDate + " 20:59:59").getTime();
+    log("唤醒有效时间段: [" + common.timestampToTime(wakeupBeginTime) + ", " + common.timestampToTime(wakeupEndTime) + "]");
+    //不在时间范围内不判断唤醒任务做没做
+    if (now < wakeupBeginTime || now >= wakeupEndTime) {
+        return;
+    }
+
+    toast("doWakeup");
+    var welcomeTips = gotoSleepEarnCoins();
+    if (welcomeTips == null) {
+        commonAction.backTaoliveMainPage();
+        return;
+    }
+
+    sleep(1000);
+    var mainFrame = welcomeTips.parent().parent();
+    var wakeupBtn = mainFrame.child(1);
+    var lucyPackageBtn = mainFrame.child(2).child(0);
+    log("睡觉按钮: " + wakeupBtn.text())
+    if (wakeupBtn.text() == "我醒来啦") {
+        var clickRet = wakeupBtn.click();
+        log("点击 我醒来啦: " + clickRet);
+        if (clickRet) {
+            common.safeSet(nowDate + ":" + wakeupTag, "done");
+            toastLog("完成 " + wakeupTag);
+
+            //关闭提示
+            var doneTips = text("完成睡觉任务").findOne(1000);
+            if (doneTips != null) {
+                var doneDlg = doneTips.parent();
+                var clickRet = doneDlg.child(doneDlg.childCount() - 1).click();
+                log("关闭 完成睡觉任务: " + clickRet);
+            }
+
+            //点福袋
+            log("福袋进度: " + lucyPackageBtn.text());
+            if (lucyPackageBtn.text() == "2/3") {
+                var clickRet = lucyPackageBtn.click();
+                sleep(1000);
+                var doneBtn = text("我知道啦").findOne(1000);
+                if (doneBtn != null) {
+                    clickRet = doneBtn.click();
+                    log("关闭 领取福袋: " + clickRet);
+                }
+            }
+        }
+    } else {
+        common.safeSet(nowDate + ":" + wakeupTag, "awake");
+        toastLog("醒着呢 " + wakeupTag);
+    }
+
+    commonAction.backTaoliveMainPage();
+}
+
+module.exports = sleepToEarn;
